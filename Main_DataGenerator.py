c0922a1fbc7fa4b80454b00932feb11040006564
Vitalii Yelizarov <v196093@gmail.com>
2024-08-28 19:07:29 +0200

    Changed the console logging style (made a separate function for that) + fixed bug in CalculateExecutionTime function

--- Main_DataGenerator.py
+++ Main_DataGenerator.py
@@ -14,18 +14,33 @@ def PathToCurrentFile():
 def GetCurrentDateTime_FormattedString():
     return str(datetime.now().strftime('%Y-%m-%d %H:%M:%S'))   
 
+def LogToConsole(Message):
+    print(GetCurrentDateTime_FormattedString() + "     " + Message)
+
 def CalculateExecutionTime(StartTime):
+    #"2024-08-28 15:43:00"
+    #StartTime = datetime(year= 2024, month= 8, day= 28, hour= 15, minute= 43, second=0)
     EndTime = datetime.now()
     ExecutionTime = EndTime - StartTime
-    ElapsedMilliseconds = int(ExecutionTime.microseconds / 1000) if (ExecutionTime.microseconds / 1000) > 99 else "0" + str(int(ExecutionTime.microseconds / 1000))
-    ElapsedSeconds = str(ExecutionTime.seconds % 60) if ExecutionTime.seconds > 9 else str("0") + str(ExecutionTime.seconds)
-    ElapsedMinutes = int(ExecutionTime.seconds / 60) if (ExecutionTime.seconds / 60) > 9 else "0" + str(int(ExecutionTime.seconds / 60))
-    ElapsedHours = int((ExecutionTime.seconds / 60) / 60) if int((ExecutionTime.seconds / 60) / 60) > 9 else "0" + str(int(((ExecutionTime.seconds / 60) / 60)))    
+    ElapsedMilliseconds = "000"
+    
+    if (ExecutionTime.microseconds % 1000) > 99:
+        ElapsedMilliseconds = int(ExecutionTime.microseconds % 1000)
+    elif (ExecutionTime.microseconds % 1000) > 9:
+        ElapsedMilliseconds = "0" + str(int(ExecutionTime.microseconds % 1000))
+    else:
+        ElapsedMilliseconds = "00" + str(int(ExecutionTime.microseconds % 1000))
+    
+    ElapsedSeconds = str(ExecutionTime.seconds % 60) if ExecutionTime.seconds % 60 > 9 else str("0") + str(ExecutionTime.seconds % 60)
+    ElapsedMinutes = int((ExecutionTime.seconds / 60) % 60) if (ExecutionTime.seconds / 60) % 60 > 9 else "0" + str(int((ExecutionTime.seconds / 60) % 60))
+    ElapsedHours = int(ExecutionTime.seconds / 60 / 60) if int(ExecutionTime.seconds / 60 / 60) > 9 else "0" + str(int(ExecutionTime.seconds / 60 / 60))
     return f"{ElapsedHours}:{ElapsedMinutes}:{ElapsedSeconds}.{ElapsedMilliseconds}"
 
 
 #App main initialization
 try:
+    StartTime = datetime.now()
+    EndTime = datetime.now()
     StartIndex = 0
     ExecutionPath = PathToCurrentFile()
     FileDirectory = ExecutionPath[StartIndex:ExecutionPath.rfind("\\") + 1]
@@ -35,8 +50,7 @@ try:
                 "PathToCSV" : FileDirectory + "TestUserData.csv",
                 "PathToLog" : FileDirectory + "Log.txt"
             }
-
-    
+        
     ValidTaxesPayerNumber_LowerValue = 1000000000
     ValidTaxesPayerNumber_MaxValue = 9999999999
     AppName = sys.argv[0]
@@ -47,10 +61,8 @@ try:
     DefaultAmount = 50
     DefaultInvalidTaxPayerRatio = 10
     DefaultOutputTo = 0
-    StartTime = datetime.now()
-    EndTime = datetime.now()
     
-    print("Process started at " + GetCurrentDateTime_FormattedString())
+    LogToConsole("Process started")
     
     for item in Arguments:
         
@@ -59,7 +71,7 @@ try:
             if value.isdigit():
                 Amount = int(value)
             else:
-                print("Parameter 'amount:' having wrong value. Using default value... \n")
+                LogToConsole("Parameter 'amount:' having wrong value. Using default value... \n")
                 Amount = DefaultAmount
         
         elif item.startswith("invalid_tax_id_ratio"):
@@ -67,7 +79,7 @@ try:
             if value.isdigit():
                 InvalidTaxPayerRatio = int(value)
             else:
-                print("Parameter 'invalid_tax_id_ratio:' having wrong value. Using default value... \n")
+                LogToConsole("Parameter 'invalid_tax_id_ratio:' having wrong value. Using default value... \n")
                 InvalidTaxPayerRatio = DefaultInvalidTaxPayerRatio
         
         # 0 - write to CSV file
@@ -78,18 +90,18 @@ try:
             if value.isdigit():
                 OutputTo = int(value)
             else:
-                print("Parameter 'invalid_tax_id_ratio:' having wrong value. Using default value... \n")
+                LogToConsole("Parameter 'invalid_tax_id_ratio:' having wrong value. Using default value... \n")
                 OutputTo = DefaultInvalidTaxPayerRatio
             
 
     if Amount == None:
-        print("Parameter 'amount:' was not found. Using default value of 50 \n")
+        LogToConsole("Parameter 'amount:' was not found. Using default value of 50 \n")
         Amount = DefaultAmount
     elif InvalidTaxPayerRatio == None:
-        print("Parameter 'invalid_tax_id_ratio:' was not found. Using default value of 10 \n")
+        LogToConsole("Parameter 'invalid_tax_id_ratio:' was not found. Using default value of 10 \n")
         InvalidTaxPayerRatio = DefaultInvalidTaxPayerRatio
     elif OutputTo == None:
-        print("Parameter 'output_to:' was not found. Using default value of 0 \n")
+        LogToConsole("Parameter 'output_to:' was not found. Using default value of 0 \n")
         InvalidTaxPayerRatio = DefaultInvalidTaxPayerRatio
     
     if InvalidTaxPayerRatio > 100:
@@ -97,7 +109,7 @@ try:
     elif InvalidTaxPayerRatio < 0:
         InvalidTaxPayerRatio = 1
             
-    print("Amount of data to be generated: " + str(Amount) + "\n")
+    LogToConsole("Amount of data to be generated: " + str(Amount) + "\n")
 
     Users = set()
     TaxesPayerNumbersSet = set()
@@ -107,12 +119,12 @@ try:
         taxes_payer_number = TaxesPayerNumberGenerator(InvalidTaxPayerRatio, ValidTaxesPayerNumber_LowerValue, ValidTaxesPayerNumber_MaxValue)
         TaxesPayerNumbersSet.add(taxes_payer_number)
 
-    print(GetCurrentDateTime_FormattedString() + "     " + "Unique tax numbers were generated")
+    LogToConsole("Unique tax numbers were generated")
     while len(PassNumbersSet) < Amount:        
         pass_number = KurwaPassNumberGenerator()
         PassNumbersSet.add(pass_number)
     
-    print(GetCurrentDateTime_FormattedString() + "     " + "Unique pass numbers were generated")
+    LogToConsole("Unique pass numbers were generated")
      
     TaxesPayerNumbersList = list(TaxesPayerNumbersSet)
     PassNumbersList = list(PassNumbersSet)
@@ -145,27 +157,27 @@ try:
         # if you would like to change it, you need to change the 20 in (Amount // 20) part (Higher value - more often you see percentage. Max value - 100)
         if (i) % (Amount // 20) == 0:
             PercentComplete = (i) * 100 // Amount
-            print( GetCurrentDateTime_FormattedString() + "     " + f"{PercentComplete}% Completed")
+            LogToConsole(f"{PercentComplete}% Completed")
 
 
     if OutputTo == 0:
         WriteInfoToFile(Users, Paths["PathToCSV"])
-        print("File with test data was successfully created and can be found in " + Paths["PathToCSV"] + "\n")
+        LogToConsole("File with test data was successfully created and can be found in " + Paths["PathToCSV"] + "\n")
     elif OutputTo == 1:
         WriteInfoToDB(Users, Paths["PathToDB"])
-        print("File with test data was successfully created and can be found in " + Paths["PathToDB"] + "\n")
+        LogToConsole("File with test data was successfully created and can be found in " + Paths["PathToDB"] + "\n")
     elif OutputTo == 2:
         WriteInfoToAllOutputSources(Users, Paths)
-        print("Files with test data was successfully created and can be found in " + FileDirectory + "\n")
+        LogToConsole("Files with test data was successfully created and can be found in " + FileDirectory + "\n")
         
 
 except Exception as ex:
-    print(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + "\t" + str(ex))
+    LogToConsole(str(ex))
     with open(Paths["PathToLog"], mode="a+", encoding="utf-8") as ErrorLog:
         ErrorLog.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')) + "\t" + str(ex))
         ErrorLog.write("\n")
 finally:
     TotalExecutionTime = CalculateExecutionTime(StartTime)
     
-    print("Process finished at " + GetCurrentDateTime_FormattedString())
-    print("Execution time: " + TotalExecutionTime + "\n")
+    LogToConsole("Process finished")
+    LogToConsole("Execution time: " + TotalExecutionTime + "\n")
